d88c500fe926f04b31eedcc24f0dd4faa77bdf93
Ivan Myasoyedov <ivan.mjasoyedov@gmail.com>
2021-11-21 18:11:31 +0200

    add summaries api delete endpoint

--- project/app/api/summaries.py
+++ project/app/api/summaries.py
@@ -29,3 +29,14 @@ async def create_summary(payload: SummaryPayloadSchema) -> SummaryResponseSchema
 
     response_object = {"id": summary_id, "url": payload.url}
     return response_object
+
+
+@router.delete("/{id}/", response_model=SummaryResponseSchema)
+async def delete_summary(id: int) -> SummaryResponseSchema:
+    summary = await crud.get(id)
+    if not summary:
+        raise HTTPException(status_code=404, detail="Summary not found")
+
+    await crud.delete(id)
+
+    return summary
